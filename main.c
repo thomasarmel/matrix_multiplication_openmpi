f8c52f99ef3e216e085270332a828140b80838f7
Thomas Prévost <thomas.prevost@etu.univ-cotedazur.fr>
2022-04-23 10:00:51 +0200

    Can gather 1D matrix

--- main.c
+++ main.c
@@ -6,9 +6,12 @@
 #include "transmission_flags.h"
 
 Matrix2D *scatter(Matrix2D *matrix2D, int num_procs, int current_rank);
+
+Matrix1D *gather(const Matrix1D *send_matrix, int num_procs, int current_rank);
+
 Matrix1D *broadcast_multiply_vector(Matrix1D *multiply_vector, int num_procs, int current_rank);
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     int rank, numprocs;
     MPI_Init(&argc, &argv);
@@ -30,10 +33,9 @@ int main(int argc, char** argv)
         }
         scattered_matrix2D = scatter(matrix2D, numprocs, rank);
         destroy_matrix2D(matrix2D);
-    }
-    else
+    } else
     {
-        scattered_matrix2D = scatter(NULL,numprocs, rank);
+        scattered_matrix2D = scatter(NULL, numprocs, rank);
     }
     printf("Process %d: ", rank);
     print_matrix2D(scattered_matrix2D);
@@ -51,20 +53,32 @@ int main(int argc, char** argv)
     printf("multiply Process %d: ", rank);
     print_matrix1D(multiply_vector);
 
+    for (int i = 0; i < multiply_vector->N; i++)
+    {
+        set_matrix1D_value(multiply_vector, i, get_matrix1D_value(multiply_vector, i) + rank);
+    }
+
+    Matrix1D *result_vector = gather(multiply_vector, numprocs, rank);
+    if (rank == 0)
+    {
+        printf("result Process %d: ", rank);
+        print_matrix1D(result_vector);
+        destroy_matrix1D(result_vector);
+    }
+
     MPI_Finalize();
 }
 
 Matrix1D *broadcast_multiply_vector(Matrix1D *multiply_vector, int num_procs, int current_rank)
 {
-    if(current_rank == 0)
+    if (current_rank == 0)
     {
         // send matrix size
         send_dim_1D(multiply_vector->N, 1, FLAG_MULTIPLY_VECTOR_BROADCAST_SIZE);
         // send matrix data
         send_matrix1D(multiply_vector, 1, FLAG_MULTIPLY_VECTOR_BROADCAST_DATA);
         return multiply_vector;
-    }
-    else
+    } else
     {
         // receive matrix size
         int size;
@@ -95,7 +109,7 @@ Matrix2D *scatter(Matrix2D *matrix2D, int num_procs, int current_rank)
         lines_per_node = num_rows / num_procs;
         // send matrix size
         send_dim_2D(num_rows, matrix2D->columns, 1, FLAG_SCATTER_SIZE);
-        for(int i = num_rows - 1; i >= 1; i--)
+        for (int i = num_rows - 1; i >= 1; i--)
         {
             send_matrix2D_line(matrix2D, 1, i, FLAG_SCATTER_DATA);
         }
@@ -110,7 +124,7 @@ Matrix2D *scatter(Matrix2D *matrix2D, int num_procs, int current_rank)
     }
     lines_per_node = num_rows / num_procs;
     Matrix2D *return_matrix2d = create_matrix2D(lines_per_node, num_cols);
-    for(int i = 0; i < (num_procs - current_rank) * lines_per_node; i++)
+    for (int i = 0; i < (num_procs - current_rank) * lines_per_node; i++)
     {
         Matrix1D *matrix1D = create_Matrix1D(num_cols);
         recv_matrix1D(matrix1D, FLAG_SCATTER_DATA);
@@ -118,12 +132,48 @@ Matrix2D *scatter(Matrix2D *matrix2D, int num_procs, int current_rank)
         if (i >= first_line)
         {
             set_matrix2D_line(return_matrix2d, lines_per_node - 1 - (i - first_line), matrix1D);
-        }
-        else
+        } else
         {
             send_matrix1D(matrix1D, current_rank + 1, FLAG_SCATTER_DATA);
             destroy_matrix1D(matrix1D);
         }
     }
     return return_matrix2d;
+}
+
+Matrix1D *gather(const Matrix1D *send_matrix, int num_procs, int current_rank)
+{
+    int lines_per_node = send_matrix->N;
+    int num_rows = lines_per_node * num_procs;
+    if (current_rank == 0)
+    {
+        Matrix1D *return_matrix = create_Matrix1D(num_rows);
+        for (int j = 0; j < lines_per_node; j++)
+        {
+            set_matrix1D_value(return_matrix, j, get_matrix1D_value(send_matrix, j));
+        }
+        for (int i = num_procs - 1; i > 0; i--)
+        {
+            Matrix1D *received_matrix = create_Matrix1D(lines_per_node);
+            recv_matrix1D(received_matrix, FLAG_GATHER_DATA);
+            for (int j = 0; j < lines_per_node; j++)
+            {
+                set_matrix1D_value(return_matrix, (i * lines_per_node) + j, get_matrix1D_value(received_matrix, j));
+            }
+            destroy_matrix1D(received_matrix);
+        }
+        return return_matrix;
+    }
+    for (int i = current_rank; i < num_procs - 1; i++)
+    {
+        // receive matrix data from rank n + 1
+        Matrix1D *received_matrix = create_Matrix1D(lines_per_node);
+        recv_matrix1D(received_matrix, FLAG_GATHER_DATA);
+        // send received matrix data to rank n - 1
+        send_matrix1D(received_matrix, current_rank - 1, FLAG_GATHER_DATA);
+        destroy_matrix1D(received_matrix);
+    }
+    // send current matrix data to rank n - 1
+    send_matrix1D(send_matrix, current_rank - 1, FLAG_GATHER_DATA);
+    return NULL;
 }
